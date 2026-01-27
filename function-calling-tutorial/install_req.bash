b8434da5479baf78fffc903d031a3bb3812e251c
FelipeUC2020 <felipe.jara.c@uc.cl>
2026-01-26 22:53:02 -0800

    langgraph helloworld

--- function-calling-tutorial/install_req.bash
+++
@@ -1,9 +0,0 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
-# 1) Make sure ipykernel is installed in the venv
-python -m pip install ipykernel
-
-# 2) Register this venv as a Jupyter kernel
-python -m ipykernel install --user --name autoredblue-venv --display-name "Python (autoredblue-venv)"
